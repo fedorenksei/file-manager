c3afc1dec32e9d38dc992784c5b0f031d0c73ee3
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-02-05 22:45:38 +0400

    feat: implement hash command

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,1 +1,23 @@
-export function calculateHash() {}
+import { createHash } from "node:crypto";
+import { getPath } from "../utils.js";
+import { createReadStream } from "node:fs";
+import { log } from "node:console";
+
+export async function calculateHash(pathToFile) {
+  pathToFile = getPath(pathToFile);
+  await new Promise((resolve, reject) => {
+    const hash = createHash("sha256");
+    createReadStream(pathToFile, "utf8")
+      .on("data", (chunk) => {
+        hash.update(chunk);
+      })
+      .on("end", () => {
+        log();
+        log(`Hash for ${pathToFile}:`);
+        console.log(hash.digest("hex"));
+        log();
+        resolve();
+      })
+      .on("error", reject);
+  });
+}
